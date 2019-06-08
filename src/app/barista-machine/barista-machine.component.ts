559dd86ab3e0d1bdfc0b1b3e0ef1b8bb5b15b44a
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2019-06-07 20:26:35 -0500

    Scaffold Barista Machine component

---
+++ src/app/barista-machine/barista-machine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barista-machine',
+  templateUrl: './barista-machine.component.html',
+  styleUrls: ['./barista-machine.component.scss']
+})
+export class BaristaMachineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
