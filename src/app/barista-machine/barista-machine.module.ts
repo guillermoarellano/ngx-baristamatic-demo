324a61dab67ea04a5a2d48b9be3a4a2faf33fc69
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2019-06-09 22:23:51 -0500

    BaristaMatic UI version 1 draft

--- src/app/barista-machine/barista-machine.module.ts
+++ src/app/barista-machine/barista-machine.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BaristaMachineComponent } from './barista-machine.component';
+// Nebular Design System
+import { NbButtonModule, NbCardModule, NbLayoutModule, NbListModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [BaristaMachineComponent],
   exports: [BaristaMachineComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, NbButtonModule, NbCardModule, NbLayoutModule, NbListModule]
 })
 export class BaristaMachineModule {}
