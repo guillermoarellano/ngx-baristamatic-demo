fcf0134bd5664db58400690476fc72c25a89474c
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2019-06-10 19:10:26 -0500

    Added animations to Inventory and Dispensing

--- src/app/barista-machine/barista-machine.component.ts
+++ src/app/barista-machine/barista-machine.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { pulse, flipInX, fadeInDown, fadeIn } from 'ng-animate';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 import { BaristaService, BaristaMenuDrink } from '../core/barista.service';
 import { IngredientQuantity } from '../core/inventory';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+const statusChange = trigger('statusChange', [
+  transition('void => *', useAnimation(pulse)),
+  transition('loop-state => start-state', []),
+  transition('* => loop-state', useAnimation(pulse))
+]);
+
+const dispensingState = trigger('dispensingState', [
+  transition(':enter', useAnimation(fadeIn, { params: { timing: 0.4, delay: 0 } }))
+]);
 
 @Component({
   selector: 'app-barista-machine',
   templateUrl: './barista-machine.component.html',
-  styleUrls: ['./barista-machine.component.scss']
+  styleUrls: ['./barista-machine.component.scss'],
+  animations: [statusChange, dispensingState]
 })
 export class BaristaMachineComponent implements OnInit {
   menuDrinks: BaristaMenuDrink[];
   inventory: IngredientQuantity[];
   loading = false;
   faCoffee = faCoffee;
+  state = 'start-state';
 
   constructor(private baristaService: BaristaService) {}
 
@@ -41,5 +56,11 @@ export class BaristaMachineComponent implements OnInit {
       this.loading = false;
       this.refresh();
     }, 3000);
+  }
+
+  onPulseAnimationDone(event: AnimationEvent) {
+    setTimeout(() => {
+      this.state === 'start-state' ? (this.state = 'loop-state') : (this.state = 'start-state');
+    }, 0);
   }
 }
